0e42830d5b13ea2c6d7071eae44c469841d31c3a
Riccardo Cavadini <wakephul@gmail.com>
2021-07-25 11:25:57 +0200

    trying to generate different spikes generators for two populations

--- main.py
+++ main.py
@@ -28,20 +28,25 @@ if __name__ == '__main__':
         
     # connection: check if spikes exist (connection: get spikes)
     from src.connection.select import select_rows
-    spikes = False
+    spikes_A = False
+    spikes_B = False
     try:
         connection = create_connection(config['app']['database'])
         if connection:
-            select_spikes_sql = spikes_queries.select_existing_spikes()
-            spikes_rows = select_rows(connection, select_spikes_sql)
-            spikes = spikes_rows[-1][1]
+            select_spikes_A_sql = spikes_queries.select_existing_spikes('A')
+            spikes_A_rows = select_rows(connection, select_spikes_A_sql)
+            spikes_A = spikes_A_rows[-1][1]
+            select_spikes_B_sql = spikes_queries.select_existing_spikes('B')
+            spikes_B_rows = select_rows(connection, select_spikes_B_sql)
+            spikes_B = spikes_B_rows[-1][1]
             close_connection(connection)
+            print
     except:
         print("Error while selecting spikes from db")
 
     # if spikes don't exist, generate spikes 
     # (--> nest: generate spikes + file: save them in json + connection: save filename in db)
-    if not spikes or (len(sys.argv) > 2 and sys.argv[2] == 'true'):
+    if (not (spikes_A and spikes_B)) or (len(sys.argv) > 2 and sys.argv[2] == 'true'):
 
         # nest: generate spikes
         from src.nest.spike_trains.spike_train_generator import poisson_spikes_generator_parrot, spike_generator_from_times
@@ -50,49 +55,51 @@ if __name__ == '__main__':
         number_of_neurons = 100
         trial_duration = stop = 1000.0 # trial duration in ms
 
-        spike_times = poisson_spikes_generator_parrot(rate, start, stop, number_of_neurons, trial_duration)
+        spikes_A_times = poisson_spikes_generator_parrot(rate, start, stop, number_of_neurons, trial_duration)
+        nest_reset() # necessary to have the network start off with a "clean" nest setup
+        spikes_B_times = poisson_spikes_generator_parrot(rate, start, stop, number_of_neurons, trial_duration)
+        nest_reset() # necessary to have the network start off with a "clean" nest setup
 
         # questo deve essere letto da N spike generators (tanti quanti i parrot neurons), il cui output diventerà l'input della rete
         # ciascuno spike generator avrà la sua lista di spike times 
         # con un nest.setStatus possiamo far generare a ciascuno spike generator gli spikes con i tempi corretti
-        nest_reset()
         
-        spikes = spike_generator_from_times(spike_times)
+        spikes_A = spike_generator_from_times(spikes_A_times)
+        spikes_B = spike_generator_from_times(spikes_B_times)
 
         import nest
-        spike_status = nest.GetStatus(spikes)
-
-        # spikes = poisson_spikes_generator(rate, start, stop)
-        
-        # N_group_A = int(brian_params['N_Excit'] * brian_params['f_Subpop_size']) # size of the excitatory subpopulation sensitive to stimulus A
-        # spikes = poisson_spikes_generator_brian(N_group_A)
-        # print("Spikes have been generated:")
-        # print(spikes)
-
-        # spikes = (1427, 1428, 1429, 1430, 1431, 1432, 1433, 1434, 1435, 1436, 1437, 1438, 1439, 1440, 1441, 1442, 1443, 1444, 1445, 1446, 1447, 1448, 1449, 1450, 1451, 1452, 1453, 1454, 1455, 1456, 1457, 1458, 1459, 1460, 1461, 1462, 1463, 1464, 1465, 1466, 1467, 1468, 1469, 1470, 1471, 1472, 1473, 1474, 1475, 1476, 1477, 1478, 1479, 1480, 1481, 1482, 1483, 1484, 1485, 1486, 1487, 1488, 1489, 1490, 1491, 1492, 1493, 1494, 1495, 1496, 1497, 1498, 1499, 1500, 1501, 1502, 1503, 1504, 1505, 1506, 1507, 1508, 1509, 1510, 1511)
+        spikes_A_status = nest.GetStatus(spikes_A)
+        spikes_B_status = nest.GetStatus(spikes_B)
 
         # file: save spikes in json
-        spikes_file_name = file_handling.save_to_file(spike_times, 'spikes/spikes_')
+        spikes_A_file_name = file_handling.save_to_file(spikes_A_times, 'spikes/spikes_A_')
+        spikes_B_file_name = file_handling.save_to_file(spikes_B_times, 'spikes/spikes_B_')
 
         # connection
         from src.connection.insert import insert_row
         try:
             connection = create_connection(config['app']['database'])
             if connection:
-                save_spike_sql = spikes_queries.insert_new_spikes()
-                values_to_insert = (spikes_file_name, 1)
-                insert_row(connection, save_spike_sql, values_to_insert)
+                save_spikes_A_sql = spikes_queries.insert_new_spikes()
+                values_to_insert = (spikes_A_file_name, 1)
+                insert_row(connection, save_spikes_A_sql, values_to_insert)
+                save_spikes_B_sql = spikes_queries.insert_new_spikes()
+                values_to_insert = (spikes_B_file_name, 1)
+                insert_row(connection, save_spikes_B_sql, values_to_insert)
                 close_connection(connection)
                 print("Spikes saved to db")
-                spike_trains = spikes
+                spike_trains_A = spikes_A
+                spike_trains_B = spikes_B
         except:
             print("Error while saving spikes to db")
 
     else:
         print('Spikes already exist:')
-        print(spikes)
+        print(spikes_A)
+        print(spikes_B)
         # file: open spikes file
-        spike_trains = file_handling.file_open(spikes)
+        spike_trains_A = file_handling.file_open(spikes_A)
+        spike_trains_B = file_handling.file_open(spikes_B)
     # nest: connect spikes to input neurons
 
 
@@ -105,11 +112,8 @@ if __name__ == '__main__':
     # # quindi se abbiamo uno spike a 250ms dovremo averlo anche a 1250ms
     # # questo lo facciamo in fase di importazione del json nella rete.
 
-    brian_params['imported_stimulus_A'] = spike_trains
-    # brian_params['imported_stimulus_B'] = spike_trains
+    brian_params['imported_stimulus_A'] = spike_trains_A
+    brian_params['imported_stimulus_B'] = spike_trains_B
     print("params", brian_params)
 
     brian_nest.run(brian_params)
-
-    # input_neurons = nest.Create('iaf_psc_exp', len(spike_trains))
-    # print(input_neurons)
--- src/queries/spikes_queries.py
+++ src/queries/spikes_queries.py
@@ -3,15 +3,17 @@ def create_spikes_table():
         CREATE TABLE IF NOT EXISTS spikes ( \
             spikeID integer PRIMARY KEY AUTOINCREMENT, \
             filename varchar(255) NOT NULL, \
+            type varchar(20) NOT NULL, \
             updated boolean NOT NULL, \
             creation timestamp DEFAULT CURRENT_TIMESTAMP \
         ); \
     "
 
-def select_existing_spikes():
+def select_existing_spikes(type):
     return " \
         SELECT * FROM spikes \
         WHERE updated = 1 \
+        AND type = " + type + " \
         ORDER BY creation ASC \
     "
 
